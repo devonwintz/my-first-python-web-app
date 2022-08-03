821dff46954fbd7567c3cff4b36e6fad3b50cb9e
devonwintz <samuelwintz@gmail.com>
2022-08-02 21:44:22 -0400

    add .flaskenv file

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
-from views.index import index_bp
-from views.about import about_bp
+# from views.index import index_bp
+# from views.about import about_bp
 
+# app = Flask(__name__)
+# app.register_blueprint(index_bp)
+# app.register_blueprint(about_bp)
+# app.run(debug=True)
+
+# Create an instance of the Flask class
 app = Flask(__name__)
-app.register_blueprint(index_bp)
-app.register_blueprint(about_bp)
-app.run(debug=True)
 
+@app.route('/')
+
+def home():
+    return 'Home';
+
+if __name__ == 'main':
+    app.run(debug=True)
 
 
